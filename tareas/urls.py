37ddc3d2d33f96f2b7287beb0071b00c1a9eba9f
AldoSZ94 <aldoszepeda94@gmail.com>
2026-08-15 13:52:04 -0600

    Modificaciones...

--- tareas/urls.py
+++ tareas/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "tareas"
 
 urlpatterns = [
-    path("lista_tareas/", views.lista_tareas, name="lista_tareas"),
+    path("", views.lista_tareas, name="lista_tareas"),
     path("nueva_tarea/", views.nueva_tarea, name="nueva_tarea"),
     path("editar_tarea/<int:tarea_id>/", views.editar_tarea, name="editar_tarea"),
     path("eliminar_tarea/<int:tarea_id>/", views.eliminar_tarea, name="eliminar_tarea"),
